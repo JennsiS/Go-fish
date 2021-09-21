0a72862f86476f6318ccc15ee88d216b033f738d
JennsiS <42972809+JennsiS@users.noreply.github.com>
2021-09-20 18:10:01 -0600

    Adding new functions to the rooms

--- rooms.py
+++ rooms.py
@@ -8,7 +8,6 @@ Integrantes:
 - Jennifer Sandoval 18962
 - Esteban del Valle 18221
 '''
-
 class Room:
 	def __init__(self, identifier, capacity, room_name):
 		self.capacity = capacity
@@ -19,14 +18,39 @@ class Room:
 		else:
 			self.name = self.identifier
 
+	# Checking if the room is full (that the number of players is equal to the capacity of the room)
 	def isFull(self):
 		if len(self.players) == self.capacity:
 			return True
 		else:
 			return False
 
+	# Adding the players to the room
 	def join(self, player):
 		if not self.is_full():
 			self.players.append(player)
 		else:
 			pass
+
+	# Removing players who leave the room
+	def leave(self, player):
+		if player in self.players:
+			self.players.remove(player)
+		else:
+			pass
+
+	# Checking if the room is empty
+	def isEmpty(self):
+		if len(self.players) == 0:
+			return True
+		else:
+			return False
+
+	# Checking if a specific player is inside the room
+	def isInRoom(self, player_identifier):
+		in_room = False
+		for player in self.players:
+			if player.identifier == player_identifier:
+				in_room = True
+				break
+		return in_room
